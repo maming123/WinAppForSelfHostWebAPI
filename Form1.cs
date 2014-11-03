bf22c97306b6e03f0e68c8304d8e9f96a0a76d92
maming123 <hurricanefbi@163.com>
2014-11-03 17:58:51 +0800

    自宿主的WebAPIDemo
    自己当做宿主的webapi 脱离了iis 相当于wcf 的服务端和客户端通信

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+using System.Windows.Forms;
+
+namespace WinAppForSelfHostWebAPI
+{
+    public partial class Form1 : Form
+    {
+        HttpSelfHostServer server =null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var config = new HttpSelfHostConfiguration(this.txtUrl.Text.Trim());
+
+            config.Routes.MapHttpRoute(
+                "API Default", "api/{controller}/{action}/{id}",
+                new { id = RouteParameter.Optional });
+
+            server = new HttpSelfHostServer(config);
+            
+            server.OpenAsync().Wait();
+               // Console.WriteLine("Press Enter to quit.");
+               // Console.ReadLine();
+            //在url中输入：http://maming.fx-func.com:8056/api/products/GetAllProducts
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (server != null)
+            {
+                server.CloseAsync().Wait();
+            }
+        }
+    }
+
+}
---
+++ WinAppCallWebAPIClient/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Http;
+
+
+namespace WinAppCallWebAPIClient
+{
+    public partial class Form1 : Form
+    {
+        static HttpClient client = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListAllProducts();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ListProduct(1);
+        }
+
+         void ListAllProducts()
+        {
+            HttpResponseMessage resp = client.GetAsync("http://maming.fx-func.com:8056/api/products/GetAllProducts").Result;
+            resp.EnsureSuccessStatusCode();
+
+            var products = resp.Content.ReadAsAsync<IEnumerable<WinAppForSelfHostWebAPI.Models.Product>>().Result;
+            foreach (var p in products)
+            {
+                this.richTextBox1.Text +="\r\n"+string.Format("{0} {1} {2} ({3})", p.Id, p.Name, p.Price, p.Category);
+            }
+        }
+         void ListProduct(int id)
+         {
+             var resp = client.GetAsync(string.Format("http://maming.fx-func.com:8056/api/products/GetProductById/{0}", id)).Result;
+             resp.EnsureSuccessStatusCode();
+
+             var product = resp.Content.ReadAsAsync<WinAppForSelfHostWebAPI.Models.Product>().Result;
+             this.richTextBox1.Text += "\r\n" + string.Format("ID {0}: {1}", id, product.Name);
+         }
+
+
+    }
+}
